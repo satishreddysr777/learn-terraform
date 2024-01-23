ccb504a82cbcc5395180e64d506ad6f2f9850230
satishreddy <satishreddysr777@gmail.com>
2024-01-23 18:49:27 -0500

    Terraform Code

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -1,3 +1,15 @@
 output "v1" {
   value = var.v1
 }
+
+output "First_value_in_list" {
+  value = var.v2[0]
+}
+
+output "Second_value_in_list" {
+  value = var.v2[1]
+}
+
+output "Value_in_map" {
+  value = var.v3["abc"]
+}
--- 02-vars/variables.tf
+++ 02-vars/variables.tf
@@ -1,3 +1,14 @@
 variable "v1" {
   default = "Hello World"
 }
+
+variable "v2" {
+  default = ["Hello", 25, true]
+}
+
+variable "v3" {
+  default = {
+    abc = 100
+    xyz = "Two Hundred"
+  }
+}
