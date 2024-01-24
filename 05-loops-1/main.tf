73d3e34dd7683067def09de53ae23b1b44d9e3ee
satishreddy <satishreddysr777@gmail.com>
2024-01-23 19:57:06 -0500

    Terraform Code

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -2,6 +2,19 @@ resource "null_resource" "test" {
   count = 10
 }
 
+# By providing count , resource will become list
+
 output "test" {
   value = null_resource.test.*.id
+}
+
+resource "aws_instance" "frontend" {
+  count                   = 3
+  ami                     = "ami-0f3c7d07486cad139"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids  = ["sg-0cd6bb8f5a75a3fb9"]
+
+  tags = {
+    Name = "test"
+  }
 }
