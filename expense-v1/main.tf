79eb5ccef6b533b689ef2f64283009d776c80e7e
satishreddy <satishreddysr777@gmail.com>
2024-01-23 19:25:57 -0500

    Terraform Code

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -8,6 +8,15 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z06671433AX1LTN7T42GS"
+  name    = "frontend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+
 resource "aws_instance" "backend" {
   ami                     = "ami-0f3c7d07486cad139"
   instance_type           = "t3.micro"
@@ -18,6 +27,15 @@ resource "aws_instance" "backend" {
   }
 }
 
+resource "aws_route53_record" "backend" {
+  zone_id = "Z06671433AX1LTN7T42GS"
+  name    = "backend-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend.private_ip]
+}
+
+
 resource "aws_instance" "mysql" {
   ami                     = "ami-0f3c7d07486cad139"
   instance_type           = "t3.micro"
@@ -26,4 +44,12 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "mysql-dev"
   }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z06671433AX1LTN7T42GS"
+  name    = "mysql-dev"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
 }
